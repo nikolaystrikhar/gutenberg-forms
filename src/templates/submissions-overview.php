82fb03bc8f881a3d514b870e10394b87a91bee12
Simon Hammes <simonhammes.27@gmail.com>
2020-03-27 12:20:08 +0100

    wrapped table in div with class wrap

--- src/templates/submissions-overview.php
+++ src/templates/submissions-overview.php
@@ -1,32 +1,37 @@
-<h1>Gutenberg Forms</h1>
+<div class="wrap">
 
-<?php if ( count( $forms ) === 0 ): ?>
+	<h1>Gutenberg Forms</h1>
+	<br>
 
-	<h3>No submissions yet.</h3>
+	<?php if ( count( $forms ) === 0 ): ?>
 
-<?php else: ?>
+		<h3>No submissions yet.</h3>
 
-	<table class="widefat striped">
-		<thead>
+	<?php else: ?>
+
+		<table class="widefat striped">
+			<thead>
 			<tr>
 				<th>Post ID</th>
 				<th>Form Title</th>
 				<th>Number of Submissions</th>
 				<th>Actions</th>
 			</tr>
-		</thead>
-		<?php foreach ( $forms as $form_id => $form ): ?>
-			<tr>
-				<td><?php echo $form_id ?></td>
-				<td><?php echo $form[ 'title' ]; ?></td>
-				<td><?php echo $form[ 'submissions' ] ?></td>
-				<td>
-					<a class="button" href="<?php echo $form[ 'edit_form_url' ]; ?>">Edit form</a>
-					<a class="button" href="<?php echo $form[ 'view_form_url' ]; ?>">View form</a><br><br>
-					<a class="button" href="<?php echo $form[ 'view_details_url' ]; ?>">View details</a>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-	</table>
+			</thead>
+			<?php foreach ( $forms as $form_id => $form ): ?>
+				<tr>
+					<td><?php echo $form_id ?></td>
+					<td><?php echo $form[ 'title' ]; ?></td>
+					<td><?php echo $form[ 'submissions' ] ?></td>
+					<td>
+						<a class="button" href="<?php echo $form[ 'edit_form_url' ]; ?>">Edit form</a>
+						<a class="button" href="<?php echo $form[ 'view_form_url' ]; ?>">View form</a><br><br>
+						<a class="button" href="<?php echo $form[ 'view_details_url' ]; ?>">View details</a>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
 
-<?php endif; ?>
+	<?php endif; ?>
+
+</div>
