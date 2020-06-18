e01c957cf92ae5a8951e4328fa60dea99978567c
ZafarKamal123 <sk4915497@gmail.com>
2020-06-18 14:38:09 +0500

    added character label in message fix #129

--- src/blocks/message/edit.js
+++ src/blocks/message/edit.js
@@ -7,18 +7,21 @@ import {
 	ResizableBox,
 	TextControl,
 	Icon,
-	RangeControl
+	RangeControl,
 } from "@wordpress/components";
 import {
 	getFieldName,
 	extract_id,
 	getEncodedData,
 	extract_admin_id,
-	get_admin_id
+	get_admin_id,
 } from "../../block/misc/helper";
 
 import { clone, set, assign } from "lodash";
-import { getRootMessages, detect_similar_forms } from "../../block/functions/index";
+import {
+	getRootMessages,
+	detect_similar_forms,
+} from "../../block/functions/index";
 import ConditionalLogic from "../../block/components/condition";
 import { TEXT_DOMAIN } from "../../block/constants";
 
@@ -27,11 +30,11 @@ const {
 	InspectorControls,
 	BlockControls,
 	BlockIcon,
-	RichText
+	RichText,
 } = wp.blockEditor;
 
 function edit(props) {
-	const handleChange = e => {
+	const handleChange = (e) => {
 		let message = e.target.value;
 
 		props.setAttributes({ message });
@@ -43,7 +46,7 @@ function edit(props) {
 		props.setAttributes({ isRequired: !isRequired });
 	};
 
-	const handleLabel = label => {
+	const handleLabel = (label) => {
 		props.setAttributes({ label });
 	};
 
@@ -62,36 +65,45 @@ function edit(props) {
 		enableCondition,
 		minimumLength,
 		maximumLength,
-		adminId
+		adminId,
 	} = props.attributes;
 
 	const getRootData = () => {
 		if (field_name === "" || detect_similar_forms(props.clientId)) {
-
 			const newFieldName = getFieldName("message", props.clientId);
 
 			props.setAttributes({
 				field_name: newFieldName,
 				adminId: {
-					value: extract_admin_id(newFieldName, 'message'),
-					default: extract_admin_id(newFieldName, 'message')
-				}
+					value: extract_admin_id(newFieldName, "message"),
+					default: extract_admin_id(newFieldName, "message"),
+				},
 			});
 			props.setAttributes({
 				id:
 					props.clientId +
 					"__" +
-					getEncodedData("message", props.clientId, isRequired, get_admin_id(adminId))
+					getEncodedData(
+						"message",
+						props.clientId,
+						isRequired,
+						get_admin_id(adminId)
+					),
 			});
 		} else if (field_name !== "") {
 			props.setAttributes({
 				id:
 					extract_id(field_name) +
 					"__" +
-					getEncodedData("message", extract_id(field_name), isRequired, get_admin_id(adminId))
+					getEncodedData(
+						"message",
+						extract_id(field_name),
+						isRequired,
+						get_admin_id(adminId)
+					),
 			});
 		}
-	}
+	};
 
 	useEffect(() => {
 		let rootMessages = getRootMessages(props.clientId, "message");
@@ -104,7 +116,6 @@ function edit(props) {
 			props.setAttributes({ messages: newMessages });
 		}
 		getRootData();
-
 	}, []);
 
 	useEffect(() => getRootData(), [props]);
@@ -117,21 +128,19 @@ function edit(props) {
 		props.setAttributes({ messages: newMessages });
 	};
 
-
 	const handleAdminId = (id) => {
 		props.setAttributes({
 			adminId: {
 				...adminId,
-				value: id.replace(/\s|-/g, "_")
-			}
-		})
-	}
+				value: id.replace(/\s|-/g, "_"),
+			},
+		});
+	};
 
 	return [
 		!!props.isSelected && (
 			<InspectorControls>
 				<PanelBody title={__("Field Settings", TEXT_DOMAIN)} initialOpen={true}>
-
 					<div className="cwp-option">
 						<TextControl
 							placeholder={adminId.default}
@@ -151,17 +160,23 @@ function edit(props) {
 							/>
 						</PanelRow>
 					) : (
-							<div className="cwp-option">
-								<p>
-									<Icon icon="info" /> {__("You cannot set a conditional field required!", TEXT_DOMAIN)}
-								</p>
-							</div>
-						)}
+						<div className="cwp-option">
+							<p>
+								<Icon icon="info" />{" "}
+								{__(
+									"You cannot set a conditional field required!",
+									TEXT_DOMAIN
+								)}
+							</p>
+						</div>
+					)}
 					{isRequired && (
 						<div className="cwp-option">
-							<h3 className="cwp-heading">{__("Required Text", TEXT_DOMAIN)}</h3>
+							<h3 className="cwp-heading">
+								{__("Required Text", TEXT_DOMAIN)}
+							</h3>
 							<TextControl
-								onChange={label =>
+								onChange={(label) =>
 									props.setAttributes({ requiredLabel: label })
 								}
 								value={requiredLabel}
@@ -170,17 +185,21 @@ function edit(props) {
 					)}
 					<div className="cwp-option">
 						<RangeControl
-							label={__('Minimum Length', TEXT_DOMAIN)}
+							label={__("Minimum Length (characters)", TEXT_DOMAIN)}
 							value={minimumLength}
 							initialPosition={0}
-							onChange={value => props.setAttributes({ minimumLength: value })}
+							onChange={(value) =>
+								props.setAttributes({ minimumLength: value })
+							}
 							min={0}
 							max={1000}
 						/>
 						<RangeControl
-							label={__('Maximum Length', TEXT_DOMAIN)}
+							label={__("Maximum Length (characters)", TEXT_DOMAIN)}
 							value={maximumLength}
-							onChange={value => props.setAttributes({ maximumLength: value })}
+							onChange={(value) =>
+								props.setAttributes({ maximumLength: value })
+							}
 							min={1}
 							max={1000}
 						/>
@@ -197,25 +216,28 @@ function edit(props) {
 				<PanelBody title={__("Messages", TEXT_DOMAIN)}>
 					{isRequired && (
 						<div className="cwp-option">
-							<h3 className="cwp-heading">{__("Required Error", TEXT_DOMAIN)}</h3>
+							<h3 className="cwp-heading">
+								{__("Required Error", TEXT_DOMAIN)}
+							</h3>
 							<TextControl
-								onChange={label => setMessages("empty", label)}
+								onChange={(label) => setMessages("empty", label)}
 								value={empty}
 							/>
 						</div>
 					)}
 					<div className="cwp-option">
-						<h3 className="cwp-heading">{__("Invalid Message Error", TEXT_DOMAIN)}</h3>
+						<h3 className="cwp-heading">
+							{__("Invalid Message Error", TEXT_DOMAIN)}
+						</h3>
 						<TextControl
-							onChange={v => setMessages("invalid", v)}
+							onChange={(v) => setMessages("invalid", v)}
 							value={invalid}
 						/>
 					</div>
 					<div className="cwp-option">
 						<p>
-							<Icon icon="info" /> {
-								__("Use {{value}} to insert field value!", TEXT_DOMAIN)
-							}
+							<Icon icon="info" />{" "}
+							{__("Use {{value}} to insert field value!", TEXT_DOMAIN)}
 						</p>
 					</div>
 				</PanelBody>
@@ -223,7 +245,7 @@ function edit(props) {
 					<div className="cwp-option">
 						<TextControl
 							label={__("Pattern (RegExp)", TEXT_DOMAIN)}
-							onChange={pattern => props.setAttributes({ pattern })}
+							onChange={(pattern) => props.setAttributes({ pattern })}
 							value={pattern}
 						/>
 					</div>
@@ -241,7 +263,12 @@ function edit(props) {
 
 			<div className="cwp-field-set">
 				<div className="cwp-label-wrap">
-					<RichText placeholder={__("Add a label", TEXT_DOMAIN)} tag="label" value={label} onChange={handleLabel} />
+					<RichText
+						placeholder={__("Add a label", TEXT_DOMAIN)}
+						tag="label"
+						value={label}
+						onChange={handleLabel}
+					/>
 					{!props.isSelected && isRequired && !enableCondition && (
 						<div className="cwp-required cwp-noticed">
 							<h3>{requiredLabel}</h3>
@@ -251,7 +278,7 @@ function edit(props) {
 				<ResizableBox
 					size={{
 						height,
-						width: "100%"
+						width: "100%",
 					}}
 					showHandle={true}
 					minHeight="50"
@@ -263,11 +290,11 @@ function edit(props) {
 						topRight: false,
 						bottomRight: false,
 						bottomLeft: false,
-						topLeft: false
+						topLeft: false,
 					}}
 					onResizeStop={(event, direction, elt, delta) => {
 						props.setAttributes({
-							height: parseInt(height + delta.height, 10)
+							height: parseInt(height + delta.height, 10),
 						});
 					}}
 				>
@@ -278,7 +305,7 @@ function edit(props) {
 					/>
 				</ResizableBox>
 			</div>
-		</div>
+		</div>,
 	];
 }
 
