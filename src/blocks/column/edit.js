f5cda2ae99f8b610be6b5473ef24ed4e32004944
ZafarKamal123 <sk4915497@gmail.com>
2020-07-03 19:50:30 +0500

    added readjustable form-columns

--- src/blocks/column/edit.js
+++ src/blocks/column/edit.js
@@ -1,16 +1,60 @@
 import React from "react";
-import { InnerBlocks } from "@wordpress/block-editor";
+import { RangeControl, PanelBody } from "@wordpress/components";
+import { TEXT_DOMAIN } from "../../block/constants/index";
+
+const { InspectorControls, InnerBlocks } = wp.blockEditor;
+const { __ } = wp.i18n;
+const $ = jQuery;
 
 function edit(props) {
-	return (
+	const { width } = props.attributes;
+	const { attributes, setAttributes, clientId } = props;
+
+	const updateAttribute = (key, value) => {
+		const currentBlockId = "#block-".concat(clientId);
+		const currentBlockElement = $(currentBlockId);
+
+		if (key === "width" && currentBlockElement.length) {
+			currentBlockElement.css("flex-basis", String(value).concat("%")); // updating the dom width
+		}
+
+		setAttributes({
+			[key]: value,
+		});
+	};
+
+	const columnStyling = {
+		flexBasis: String(width).concat("%"),
+	};
+
+	return [
 		<div className="cwp-col">
 			<InnerBlocks
-				className="cwp-col_inserter"
 				templateLock={false}
+				style={{
+					backgroundColor: "red",
+				}}
+				className="cwp-col_inserter"
 				renderAppender={() => <InnerBlocks.ButtonBlockAppender />}
 			/>
-		</div>
-	);
+		</div>,
+		null,
+		!!props.isSelected && (
+			<InspectorControls>
+				<PanelBody
+					initialOpen={true}
+					title={__("Column Settings", TEXT_DOMAIN)}
+				>
+					<RangeControl
+						value={width}
+						allowReset
+						label={__("Width (%)", TEXT_DOMAIN)}
+						onChange={(newWidth) => updateAttribute("width", newWidth)}
+					/>
+				</PanelBody>
+			</InspectorControls>
+		),
+	];
 }
 
 export default edit;
--- src/blocks/column/save.js
+++ src/blocks/column/save.js
@@ -2,8 +2,14 @@ import React from "react";
 import { InnerBlocks } from "@wordpress/block-editor";
 
 function save(props) {
+	const width = String(props.attributes.width).concat("%");
+
+	const styling = {
+		flexBasis: width,
+	};
+
 	return (
-		<div className="cwp-col">
+		<div className="cwp-col" style={styling}>
 			<InnerBlocks.Content />
 		</div>
 	);
