684fd37fe0c937efc354f204363c331209ca718e
ZafarKamal123 <sk4915497@gmail.com>
2020-07-04 13:48:11 +0500

    updates

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -283,7 +283,7 @@ class Dashboard
 
                 // for testing purpose...
 
-                $production = true;
+                $production = false;
 
                 if ($production) {
                     $js = "http://localhost:8080/gutenbergforms/wp-content/plugins/gutenberghub-dashboard/build/build.js";
