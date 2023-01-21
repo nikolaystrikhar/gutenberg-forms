527806ce242d67a23ddc7ba203b7facdd5838724
Krystian Kat <krystian.kat@protonmail.com>
2023-01-21 16:02:09 +0100

    feat: convert column block to server side

--- src/Core/BlockHandler.php
+++ src/Core/BlockHandler.php
@@ -6,11 +6,10 @@ use GutenbergForms\Core\Blocks\Email;
 use GutenbergForms\Core\Blocks\Textarea;
 
 // TODO update Mocked Classes
-// use GutenbergForms\Core\Blocks\Block;
 // use GutenbergForms\Core\Blocks\Button;
 // use GutenbergForms\Core\Blocks\Calculation;
 use GutenbergForms\Core\Blocks\Checkbox;
-// use GutenbergForms\Core\Blocks\Column;
+use GutenbergForms\Core\Blocks\Column;
 // use GutenbergForms\Core\Blocks\Components;
 // use GutenbergForms\Core\Blocks\Datepicker;
 // use GutenbergForms\Core\Blocks\File;
@@ -49,11 +48,10 @@ class BlockHandler {
 		Textarea::init();
 		Email::init();
 		// TODO update Mocked Classes
-		// Block::init();
 		// Button::init();
 		// Calculation::init();
 		Checkbox::init();
-		// Column::init();
+		Column::init();
 		// Components::init();
 		// Datepicker::init();
 		// File::init();
--- src/blocks/column/index.js
+++ src/blocks/column/index.js
@@ -1,27 +1,24 @@
-const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
+const { __ } = wp.i18n;
 
 import columnEdit from "./edit.js";
-import columnSave from "./save.js";
-
-import blockData from "./block.json";
+import { title, attributes } from "./block.json";
+import { deprecated } from "./deprecated/deprecated";
 
 // Child block for the form-column block for creating layouts
 
-const { title, attributes } = blockData;
-
 registerBlockType("cwp/column", {
-	title: __(title),
+	title: __(title, "forms-gutenberg"),
 	icon: "editor-table",
 	category: "gutenberg-forms",
 	keywords: [
-		__("gutenberg-forms"),
-		__("forms"),
-		__("form-column"),
-		__("column"),
+		__("gutenberg-forms", "forms-gutenberg"),
+		__("forms", "forms-gutenberg"),
+		__("form-column", "forms-gutenberg"),
+		__("column", "forms-gutenberg"),
 	],
-	edit: columnEdit,
-	save: columnSave,
 	attributes,
+	edit: columnEdit,
 	parent: ["cwp/form-column"],
+	deprecated: deprecated,
 });
